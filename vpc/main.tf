8d9f3b422d1a8c2601ef69e72d7c39cd4b5f86df
Ramanarayana <gunjirama1999@gmail.com>
2023-08-13 06:11:03 +0530

    tag created

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "this"{
   cidr_block       = local.cidr
   instance_tenancy = "default"
+  tags = var.tags
 }
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -2,4 +2,9 @@
 variable  "cidr"{
 type = string
 }
-//default = "10.0.0.0/16"
+variable "tags"{
+     type = map
+     default = {
+      Name = vpc
+      }
+  }
